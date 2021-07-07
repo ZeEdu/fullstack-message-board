0665caed4322a888f19b3ba7177d72997063a9dc
eduardo_Siqueira <eduardo.carlos.siqueira@gmail.com>
2021-07-07 09:05:24 -0300

    makes ctx optional at GetApiClient function

--- services/axios.tsx
+++ services/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { parseCookies } from "nookies";
 
-export function GetApiClient(ctx) {
+export function GetApiClient(ctx?) {
   const { ["messageboard.token"]: token } = parseCookies(ctx);
 
   const api = axios.create({
